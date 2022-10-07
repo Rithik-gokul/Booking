b3f3fe7733295b216dbabe596c72f5fe4b69036f
Gokul Rithik <rithikgokul200@gmail.com>
2022-10-07 18:10:57 +0530

    Updates

---
+++ precache-manifest.50967bca99a3f154f1a7d2bc8417c761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c31f3fdcf53a12b89bfd",
+    "url": "/Booking/static/css/main.e9bde942.chunk.css"
+  },
+  {
+    "revision": "c31f3fdcf53a12b89bfd",
+    "url": "/Booking/static/js/main.c31f3fdc.chunk.js"
+  },
+  {
+    "revision": "84e63534eb4cc4246c91",
+    "url": "/Booking/static/js/1.84e63534.chunk.js"
+  },
+  {
+    "revision": "a4cdb7a13528b1866553",
+    "url": "/Booking/static/js/runtime~main.a4cdb7a1.js"
+  },
+  {
+    "revision": "9f9a93113dc2ff5c7ba9e2d7d8b1b234",
+    "url": "/Booking/static/media/star.9f9a9311.jpg"
+  },
+  {
+    "revision": "ca82cc44fe1fc65840447f9194c26904",
+    "url": "/Booking/index.html"
+  }
+];
